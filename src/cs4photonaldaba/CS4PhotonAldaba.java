dbca998a39394df6a92764fa43d41673d32436f8
PHOTON <PHOTON@DESKTOP-LVE5NC0>
2023-08-30 16:03:49 +0800

    first java project

---
+++ src/cs4photonaldaba/CS4PhotonAldaba.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4photonaldaba;
+
+/**
+ *
+ * @author PHOTON
+ */
+public class CS4PhotonAldaba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       String greeting = "Hi";
+       System.out.println(greeting + " Hello");
+    }
+    
+}
